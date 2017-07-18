02b57695e0bd7be70c12e394a59ec6c19bece86a
Siu <minlicp@hotmail.co.uk>
2017-07-18 16:57:47 +0100

    Ex8.rb added

---
+++ Ex8.rb
@@ -0,0 +1,15 @@
+# vairable holds 4 sub values
+formatter = "%{first} %{second} %{third} %{fourth}"
+# display vairable with each defined sub values
+puts formatter % {first: 1, second: 2, third: 3, fourth: 4}
+puts formatter % {first: "one", second: "two", third: "three", fourth: "four"}
+puts formatter % {first: true, second: false, third: true, fourth: false}
+# display all sub values in the vairable 4 times
+puts formatter % {first: formatter, second: formatter, third: formatter, fourth: formatter}
+# another way to write code as line 4 to 7
+puts formatter % {
+  first: "I had this thing.",
+  second: "That you could type up right.",
+  third: "But it didn't sing.",
+  fourth: "So I said goodnight."
+}
