d387d4c048dc32a9d91e1c89ca46305dbe5761e6
Siu <minlicp@hotmail.co.uk>
2017-07-18 16:15:36 +0100

    Ex6.rb added

---
+++ Ex6.rb
@@ -0,0 +1,26 @@
+#store value 10 in vairable
+types_of_people = 10
+#store string and include value from types_of_people
+x = "There are #{types_of_people} types of people."
+#store string binary in vairable binary
+binary = "binary"
+do_not = "don't"
+#store string and include vairable binary and do_not
+y = "Those who know #{binary} and those who #{do_not}."
+#display both x and y string
+puts x
+puts y
+#add string then display string from vairable
+puts "I said: #{x}."
+puts "I also said: '#{y}'."
+#boolean value in vairable
+hilarious = false
+#a boolean vaue at the end of string
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+#display string
+puts joke_evaluation
+#store string in vairable
+w = "This is the left side of..."
+e = "a string with a right side."
+#display both vairable together
+puts w + e
