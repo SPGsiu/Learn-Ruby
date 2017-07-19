8bf51647f3c777f66a7cbe9aee9669aa63c9bb67
Siu <minlicp@hotmail.co.uk>
2017-07-19 09:15:50 +0100

    Ex1.rb Extended

--- Ex1.rb
+++ Ex1.rb
@@ -5,4 +5,8 @@ puts "This is fun."
 puts "Yay! Printing."
 puts "I'd much rather you 'not'."
 puts 'I "said" do not touch this.'
-puts "Got this"
+
+puts "got this"
+print "line"
+puts "up"
+puts "line up"
