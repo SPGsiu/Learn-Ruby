d718b2f1edd2f7c061ce704dbad4dc253e3fb8b7
Siu <minlicp@hotmail.co.uk>
2017-07-19 10:14:54 +0100

    Ex5.rb Extended

--- Ex5.rb
+++ Ex5.rb
@@ -15,3 +15,13 @@ puts "His teeth are usually #{teeth} depending on the coffee."
 
 # this line is tricky, try to get it exactly right
 puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
+
+# get user to enter data to produce different results
+puts "Enter your name"
+user_name = gets.chomp
+puts "Enter you age"
+user_age = gets.to_i
+puts "Enter your city"
+user_city = gets.chomp
+user_born = (2017 - user_age)
+print "your name is #{user_name} your age is #{user_age} you live in #{user_city} you must be born in #{user_born}"
